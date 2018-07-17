f3309dccf022873d84e7db84110e60a3c6d5f747
tammy0508 <tamara.dandreamatteo@gmail.com>
2018-07-17 15:24:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,9 +5,13 @@ class App < Sinatra::Base
   get '/' do
     "Welcome to your app!!!! "
   end
-  
+
   get '/name' do
     "Hello Tamara"
   end
-  
+
+  get '/blog_:lelia' do
+    "the text you wrote"
+  end
+
 end
